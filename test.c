f79127cd19ba8b19c44e91ba4dd5d5253e5ce0a4
versun <versun.pan@gmail.com>
2021-03-03 14:36:24 +0800

    just a new test

--- test.c
+++ test.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    char * p1 = "Klingon";    
-    p1[0] = 'F';
-    printf("Klingon");
-    printf(": Beware the %ss!\n","Klingon");
+    printf("%li,%li",sizeof(char),sizeof(char*));
     return 0;
 }

975c147f14821f7edbca813c2c36230a07725e7b
versun <versun.pan@gmail.com>
2021-03-05 11:42:05 +0800

    complete 13.11-10.c

---
+++ 13/13.11-10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    FILE* fp1;
+    char filename[80];
+    long int location;
+    char c;
+    puts("Inpute a filename: ");
+    scanf("%s",filename);
+    if((fp1=fopen(filename,"r")) == NULL){
+        printf("Can't open %s file",filename);
+        exit(EXIT_FAILURE);
+    }
+    puts("Input a number to positioning:");
+    scanf("%li",&location);
+    fseek(fp1, location, SEEK_SET);
+    c = getc(fp1); 
+    while(!feof(fp1) && c!='\n'){
+        printf("%c",c);
+        c = getc(fp1); 
+    }
+    puts("Done!");
+    fclose(fp1);
+    return 0;
+}
